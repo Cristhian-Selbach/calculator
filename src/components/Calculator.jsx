7e330fab5c892a6b42ff16a5df0a3ed7a376544c
Cristhian Selbach <crsthian.04@gmail.com>
2025-01-24 20:57:41 -0300

    design of calculator finished

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -6,31 +6,40 @@ export default function Calculator() {
       <div className="carcass">
         <div className="visor">
           <div className="previus-operand">123</div>
-          <div className="current-operand">123</div>
+          <div className="current-operand">
+            <div className="result-equals">=</div>
+            112121+3211111111111111111111113
+          </div>
         </div>
+
+        <div className="utilities-grid">
+          <button className="utilities">e</button>
+          <button className="utilities">µ</button>
+          <button className="utilities">sin</button>
+          <button className="utilities">cos</button>
+        </div>
+
         <div className="calculator-grid">
-          <button>e</button>
-          <button>µ</button>
-          <button>sin</button>
-          <button>cos</button>
-          <button>Ac</button>
-          <button>del</button>
-          <button>÷</button>
-          <button>*</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>-</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>+</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button className="remove">Ac</button>
+          <button className="remove">del</button>
+          <button className="operations">÷</button>
+          <button className="operations">*</button>
+          <button className="numbers">7</button>
+          <button className="numbers">8</button>
+          <button className="numbers">9</button>
+          <button className="operations">-</button>
+          <button className="numbers">4</button>
+          <button className="numbers">5</button>
+          <button className="numbers">6</button>
+          <div className="vertical-high">
+            <button className="operations plus">+</button>
+            <button className="equal">=</button>
+          </div>
+          <button className="numbers">1</button>
+          <button className="numbers">2</button>
+          <button className="numbers">3</button>
+          <button className="horizontal-high numbers">0</button>
+          <button className="numbers">.</button>
         </div>
       </div>
     </>
